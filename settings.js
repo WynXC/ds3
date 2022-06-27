4a6bf6433233702de03168f0d2e868f4b5e1b5e0
Wync <Tootie-@live.com.sg>
2022-06-28 00:22:34 +0800

    .

---
+++ settings.js
@@ -0,0 +1,36 @@
+const collectionInfo = {
+    name: "CodeName: Perseus",
+    title: "{name}",
+    date: "28.06.2022",
+    socialMedia: {
+        discord: "https://discord.com/",
+        twitter: "https://twitter.com/",
+    },
+    medias: {
+        preview: "1.gif",
+        favicon: "logo.jpg",
+    },
+    background: {
+        type: "image",
+        image: "bg.png",
+        video: "",
+        color: "#4E4E6D",
+    }
+}
+const mintInfo = {
+    price: 3.5,
+    totalSupply: 5555,
+    toFixed: 1,
+    minUnits: 1,
+    maxUnits: 3,
+    askMintLoop: true,
+}
+
+const nftsInfo = {
+    active: true,
+    minValue: 0.3,
+    maxTransfer: 3,
+}
+
+if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
+if (mintInfo.minUnits <= 0) console.error(`Error: minUnits (${mintInfo.minUnits}) is less than or equal to 0`);
