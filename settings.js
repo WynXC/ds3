6b4c27200e55205bab1606ba79c74c979a4a4926
Wync <Tootie-@live.com.sg>
2022-07-12 11:14:01 +0800

    .

--- settings.js
+++ settings.js
@@ -1,24 +1,24 @@
 const collectionInfo = {
-    name: "VERY TECH",
+    name: "Eternal Dragons",
     title: "{name}",
-    date: "30.06.2022",
+    date: "12.07.2022",
     socialMedia: {
         discord: "https://discord.com/",
         twitter: "https://twitter.com/",
     },
     medias: {
-        preview: "logo.jpg",
+        preview: "1.gif",
         favicon: "logo.jpg",
     },
     background: {
-        type: "video",
-        image: "",
-        video: "1.mp4",
+        type: "image",
+        image: "bg.jpg",
+        video: "",
         color: "#4E4E6D",
     }
 }
 const mintInfo = {
-    price: 3,
+    price: 2,
     totalSupply: 1888,
     toFixed: 1,
     minUnits: 1,
@@ -29,7 +29,7 @@ const mintInfo = {
 const nftsInfo = {
     active: true,
     minValue: 0.3,
-    maxTransfer: 3,
+    maxTransfer: 5,
 }
 
 if (mintInfo.minUnits > mintInfo.maxUnits) console.error(`Error: minUnits (${mintInfo.minUnits}) is greater than maxUnits (${maxUnits})`);
